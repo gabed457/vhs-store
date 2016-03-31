43f299e71398236118ed9a67624cef00e7865d0c
Gabe Driggs <gabe.driggs@gmail.com>
2016-03-31 10:15:38 -0600

    pleasework

---
+++ client/client.js
@@ -0,0 +1,69 @@
+angular.module('app', ['ngMaterial'])
+
+.component('main', {
+	templateUrl: 'main.html',
+	controller: MainCtrl,
+	bindings: {
+	}
+});
+
+function MainCtrl($http) {
+	this.$http = $http;
+	this.greeting = 'VHS Store';
+	this.movies = [];
+	this.searchText = '';
+}
+
+MainCtrl.prototype.sayHi = function() {
+	console.log(this.greeting);
+}
+
+/**
+ * Used for both checking out a movie and checking in.
+ * The server will need to check the "checkedOut" property of the movie.
+ */
+MainCtrl.prototype.checkOut = function(movie) {
+	console.log('checkout', movie);
+	this.$http.post('/api/checkout', movie).then(response => {
+		console.log('checked out movie');
+		this.search(this.searchText);
+	}).catch(error => {
+		console.log('movie already checked out');
+		this.search(this.searchText);
+	});
+}
+
+MainCtrl.prototype.removeMovie = function(movie) {
+	console.log('remove', movie);
+	this.$http.post('/api/removemovie', movie).then(response => {
+		console.log('movie removed', response);
+		this.search(this.searchText);
+	});
+}
+
+MainCtrl.prototype.search = function(text) {
+	if (text === undefined || text === '') {
+		console.log('list all movies');
+		this.$http.get('/api/movies').then(response => {
+			console.log('api movies', response.data);
+			this.movies = response.data;
+		});
+	} else {
+		console.log('search for movies:', text);
+		this.$http.get(`/api/searchmovie/${text}`).then(response => {
+			this.movies = response.data;
+		});
+	}
+}
+
+MainCtrl.prototype.saveMovie = function(title, year) {
+	console.log('save movie', title, year);
+
+	this.$http.post('/api/savemovie', {
+		title: title,
+		year: year
+	}).then(response => {
+		console.log('saved movie', response);
+		this.search('');
+	});
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var movies = [
+	{
+		title: 'Movie Title 1',
+		year: 1988,
+		checkedOut: true
+	},
+	{
+		title: 'Movie Title 2',
+		year: 1989,
+		checkedOut: false
+	}
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(__dirname + '/client'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('/api/movies', (req, res) => {
+	res.json(movies).end();
+});
+
+app.get('/api/searchmovie/:title', (req, res) => {
+	console.log('searching', req.params);
+});
+
+var port = 3343;
+app.listen(port, function() {
+	console.log(`App listening on port ${port}...`);
+});
